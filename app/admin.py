0f579cf090f0c092081ee6338677d7c2cd920cb0
Ramya <ramyanivaskannan@gmail.com>
2024-01-10 12:51:31 +0530

    first commit

--- app/admin.py
+++ app/admin.py
@@ -3,8 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from app.models import *
 
+class customizeWebpage(admin.ModelAdmin):
+  list_display=['topic_name','name','url','email']
+  list_display_links=['url']
+  list_editable=['email']
+  list_filter=['name']
+  #list_per_page=2
+  search_fields=['name']
+  #list_max_show_all=2
+  list_select_related=['topic_name']
+
+
 admin.site.register(Topic)
 
-admin.site.register(Webpage)
+admin.site.register(Webpage,customizeWebpage)
 
 admin.site.register(AccessRecord)
